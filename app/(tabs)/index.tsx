762d383fbad6d24af742903f6e7b02aa7ac93ecf
kei_creativelife <kenken19920616@gmail.com>
2023-11-18 23:41:16 +0900

    ログイン表示の修正

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Page = () => {
+  const { isSignedIn } = useAuth();
   return (
     <View>
-      <Link href={"/(modals)/login"}>Login</Link>
+      {!isSignedIn && <Link href={"/(modals)/login"}>Login</Link>}
       <Link href={"/(modals)/booking"}>Booking</Link>
       <Link href={"/listing/1337"}>Listing</Link>
     </View>
