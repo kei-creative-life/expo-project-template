cb1e218d71d1cb2b92e93954eeab86159eeb92b7
kei_creativelife <kenken19920616@gmail.com>
2023-11-19 13:58:47 +0900

    アイテム一覧＆詳細の表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { Stack } from "expo-router";
 import ExploreHeader from "@/components/ExploreHeader";
 import Listings from "@/components/Listings";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
-  const [category, setCategory] = React.useState<string>("Technology");
+  const items = useMemo(() => listingsData as any, []);
+  const [category, setCategory] = useState<string>("Technology");
   const onDataChanged = (category: string) => {
     setCategory(category);
   };
@@ -17,7 +19,7 @@ const Page = () => {
           header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
         }}
       />
-      <Listings listings={[]} category={category} />
+      <Listings listings={items} category={category} />
     </View>
   );
 };
